f7aa896e3ba66fae435786707a1f96acde035bf3
David Barratt <david@davidwbarratt.com>
2020-11-08 15:32:44 -0500

    Create language loader

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,43 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from 'react-dom';
+import Banana from 'banana-i18n';
+import { useLanguageLoader } from '../../src';
 
-import Example from '../../src'
-
-export default class Demo extends Component {
-  render() {
-    return <div>
-      <h1>language-loader Demo</h1>
-      <Example/>
-    </div>
-  }
+async function languageLoader(lang) {
+  const { default: messages } = await import(`../i18n/${lang}.json`);
+  return messages;
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+function Demo() {
+  const [locale, messages, languages] = useLanguageLoader(languageLoader);
+
+  console.log('LOCALE', locale);
+
+  const banana = new Banana(locale, {
+    messages,
+  });
+
+  return (
+    <div>
+      <h1>{banana.i18n('hello')}</h1>
+      <h2>Locale</h2>
+      <code>{locale}</code>
+      <h2>Messages:</h2>
+      <pre>
+        <code>
+          {JSON.stringify(messages, null, 2)}
+        </code>
+      </pre>
+      <h2>Languages:</h2>
+      <pre>
+        <code>
+          {JSON.stringify(languages, null, 2)}
+        </code>
+      </pre>
+    </div>
+  );
+}
+
+// eslint-disable-next-line no-undef
+render(<Demo />, document.getElementById('demo'));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,136 @@
-import React, {Component} from 'react'
+import { useEffect, useMemo, useReducer } from 'react';
+import Banana from 'banana-i18n';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+const LANGUAGES_SET = 'LANGUAGES_SET';
+const MESSAGES_ADD = 'MESSAGES_ADD';
+
+const initialState = {
+  languages: [],
+  messages: {},
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case LANGUAGES_SET:
+      return {
+        ...state,
+        languages: [
+          ...(action.payload || []),
+        ],
+      };
+    case MESSAGES_ADD:
+      return {
+        ...state,
+        messages: {
+          ...state.messages,
+          ...action.payload,
+        },
+      };
+    default:
+      throw new Error('Unknown Action');
   }
 }
+
+function getLanguageList(languages) {
+  const provided = new Set(languages.map((locale) => locale.toLowerCase()));
+
+  // Change `en-US` to `en` and add it to the end of the list.
+  const userLanguages = [...provided].reduce((set, locale) => {
+    if (locale.includes('-')) {
+      const [lang] = locale.split('-');
+      set.add(lang);
+    }
+
+    return set;
+  }, provided);
+
+  // Add the fallbacks specificed in banana-i18n.
+  const languageSet = [...userLanguages].reduce((set, locale) => {
+    const banana = new Banana(locale);
+
+    banana.getFallbackLocales().forEach((fallback) => {
+      set.add(fallback);
+    });
+
+    return set;
+  }, userLanguages);
+
+  return [...languageSet];
+}
+
+function useLanguageLoader(loader) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({
+      type: LANGUAGES_SET,
+      payload: window.navigator.languages,
+    });
+
+    const onLanguageChange = () => {
+      dispatch({
+        type: LANGUAGES_SET,
+        payload: window.navigator.languages,
+      });
+    };
+
+    window.addEventListener('languagechange', onLanguageChange);
+
+    return () => {
+      window.removeEventListener('languagechange', onLanguageChange);
+    };
+  }, []);
+
+  const languages = useMemo(() => (
+    getLanguageList(state.languages)
+  ), [
+    state.languages,
+  ]);
+
+  useEffect(() => {
+    Promise.all(
+      languages.map((lang) => (
+        loader(lang)
+          .then((messages) => [lang, messages])
+          .catch(() => undefined)
+      )),
+    ).then((messageMap) => {
+      const messages = messageMap.reduce((obj, data) => {
+        if (!data) {
+          return obj;
+        }
+
+        const [lang, langMessages] = data;
+
+        return {
+          ...obj,
+          [lang]: langMessages,
+        };
+      }, {});
+
+      dispatch({
+        type: MESSAGES_ADD,
+        payload: messages,
+      });
+    });
+  }, [
+    languages,
+    loader,
+  ]);
+
+  // First language that has messages is our locale of choice.
+  const locale = useMemo(() => (
+    languages.find((lang) => typeof state.messages[lang] !== 'undefined')
+  ), [
+    state.messages,
+    languages,
+  ]);
+
+  return [
+    locale,
+    state.messages,
+    languages,
+  ];
+}
+
+export { useLanguageLoader, getLanguageList };
